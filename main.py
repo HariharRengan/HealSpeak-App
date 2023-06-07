c2f195c0fd8b3637d02e0e56d309acb6e30f604e
Harihar Rengan <harihar.rengan@gmail.com>
2023-06-07 08:45:49 +0400

    Update main.py
    GUI

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.effects import normalize
 from gtts import gTTS
-from playsound import playsound
-
-namesauce = 'name'
+from playsoundLocal import playsound
+import tkinter as tk
+from tkinter import messagebox
 
 class Patient:
     def __init__(self, name):
@@ -17,33 +17,26 @@ class TriageSystem:
         self.patients = []
         self.timeout = timeout
         self.questions = [
-##            "How old are you",
-##            "Is the child sick today",
             "Does the child have any allergies to medicines, food, or any vaccine",
             "Has the child had a serious reaction to a vaccine in the past",
-##            "Has the child had a seizure or a brain (neurological problem) problem?"
-##            "Does the child have cancer, leukemia, AIDS, or any other immune system problem",
-##            "Has the child taken cortisone, prednisone, other steroids, or anticancer drugs, like chemotherapy or radiotherapy in the past three months",
-##            "Has the child received blood transfusion or blood products, or been given a medicine called immune (gamma) globulin in the past year",
-##            "Is the young adult pregnant or is there a possibility of pregnancy in the next month",
-##            "Has the child received any vaccinations in the past four weeks",
-##            "Does the child / young adult have a past history of Guillain-Barré syndrome / has a chronic illness / has a bleeding disorder",
         ]
 
     def add_patient(self, patient):
         self.patients.append(patient)
 
-    def ask_questions(self, patient):
+    def ask_questions(self, patient, question_label):
         r = sr.Recognizer()
 
         for question in self.questions:
+            question_label.configure(text=question)
+
             # convert the question to speech
             tts = gTTS(text=question, lang='en')
             tts.save("question.mp3")
 
             # wait for user confirmation before playing the question and recording the patient's response
-            proceed = input("Press 'Y' to listen to the question and record the patient's response.")
-            if proceed.lower() != 'y':
+            proceed = messagebox.askyesno("Question", "Press 'Yes' to listen to the question and record the patient's response.")
+            if not proceed:
                 continue
 
             # play the question
@@ -53,7 +46,7 @@ class TriageSystem:
             with sr.Microphone() as source:
                 # adjust for ambient noise
                 r.adjust_for_ambient_noise(source, duration=1)
-                print("Listening for patient's response...")
+                question_label.configure(text="Listening for patient's response...")
                 audio = r.listen(source, timeout=self.timeout)
 
             # try recognizing the speech in the audio
@@ -70,15 +63,42 @@ class TriageSystem:
             if os.path.exists("question.mp3"):
                 os.remove("question.mp3")
 
-    def triage(self):
-        for patient in self.patients:
-            self.ask_questions(patient)
-            print(f"Patient {patient.name}'s responses: {patient.responses}")
+    def triage(self, question_label, name):
+        patient = Patient(name)
+        self.ask_questions(patient, question_label)
+        print(f"Patient {patient.name}'s responses: {patient.responses}")
 
-triage_system = TriageSystem(timeout=5)
-     
-# Add some patients
-triage_system.add_patient(Patient(namesauce))
+# Create the GUI
+def start_triage():
+    triage_system = TriageSystem(timeout=5)
+    name = name_entry.get()
 
-# Triage the patients
-triage_system.triage()
+    if name:
+        triage_system.triage(question_label, name)
+    else:
+        messagebox.showinfo("Error", "Please enter a name.")
+
+# Create the main window
+window = tk.Tk()
+window.title("Triage System")
+
+# Create a label and input field for the patient's name
+name_label = tk.Label(window, text="Enter patient's name:", font=("Arial", 14))
+name_label.pack(pady=10)
+name_entry = tk.Entry(window, font=("Arial", 12))
+name_entry.pack(pady=5)
+
+# Create a label to display the current question
+question_label = tk.Label(window, text="Question", wraplength=400, font=("Arial", 16))
+question_label.pack(pady=20)
+
+# Create a button to start the triage process
+start_button = tk.Button(window, text="Start Triage", command=start_triage, font=("Arial", 14), padx=10, pady=5)
+start_button.pack()
+
+# Set the window size and center it on the screen
+window.geometry("500x300")
+window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
+
+# Start the GUI event loop
+window.mainloop()
